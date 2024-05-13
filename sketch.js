71680439fe05b617dc931785b57527715b4db453
raulane <168672247+raulane@users.noreply.github.com>
2024-05-13 08:28:53 -0300

    adicionando arquivo

---
+++ sketch.js
@@ -0,0 +1,14 @@
+nctioncccc  setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+  stroke ("blue");
+    fill("red");
+    
+    if(mouseIsPressed){
+    rect(mouseX,mouseY,20,35);
+  }   
+  }  
+    
